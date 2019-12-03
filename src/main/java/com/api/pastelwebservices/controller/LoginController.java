e4c7b535058197d80dd1ca04f4f5f2b19215eb7c
abraham_git_portable <abrmendoza32@gmail.com>
2019-12-03 10:17:18 -0500

    nueva base de datos azure creado - crossOrigin agregado

--- src/main/java/com/api/pastelwebservices/controller/LoginController.java
+++ src/main/java/com/api/pastelwebservices/controller/LoginController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import com.api.pastelwebservices.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/login")
+@CrossOrigin(origins = "*")
 public class LoginController {
 	
 	@Autowired
