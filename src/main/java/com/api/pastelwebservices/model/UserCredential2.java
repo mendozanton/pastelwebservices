cb025cba6fbe27c2f68b1dac1800c134577ac0fe
abraham <abrmendoza32@gmail.com>
2020-01-22 18:57:43 -0500

    add captcha actualizado  2

--- src/main/java/com/api/pastelwebservices/model/UserCredential2.java
+++ src/main/java/com/api/pastelwebservices/model/UserCredential2.java
@@ -3,6 +3,10 @@ package com.api.pastelwebservices.model;
 public class UserCredential2 {
 	private String email;
 	private String password;
+	
+	
+	public UserCredential2() {
+	}
 	public UserCredential2(String email, String password) {
 		this.email = email;
 		this.password = password;
